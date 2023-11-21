c7a7efecbf99f9a5257afc2ba92920731edd5096
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2023-11-21 20:19:36 +0900

    Title: 공약수의 유무, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231121/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = a; i <= b; i++) {
+            if (1920 % i == 0 & 2880 % i == 0)
+                System.out.println(1);
+                return;
+        }
+
+        System.out.println(0);
+    }
+}
