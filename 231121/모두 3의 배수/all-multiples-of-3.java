e0634470059c3eb4ecedab841d76c511ee217715
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2023-11-21 20:24:02 +0900

    Title: 모두 3의 배수, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231121/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean flag = true;
+
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+
+            if (n % 3 != 0)
+                flag = false;
+        }
+
+        System.out.println(flag ? 1 : 0);
+    }
+}
