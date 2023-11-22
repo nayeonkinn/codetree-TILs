692798e664fe93e59e3b9fc20d0315a3d6ed66db
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2023-11-22 17:59:29 +0900

    Title: 9-1 정사각형, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 231122/9-1 정사각형/9-1-square.java
+++ 231122/9-1 정사각형/9-1-square.java
@@ -4,15 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n, num = 9;
+        int n, num = 0;
 
         n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (num == 0) num = 9;
-                System.out.print(num--);
-            }
+            for (int j = 0; j < n; j++)
+                System.out.print(9 - (num++ % 9));
             System.out.println();
         }
     }
