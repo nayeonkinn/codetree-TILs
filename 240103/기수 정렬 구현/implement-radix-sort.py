b4c788d0c63c476609ea6deb25de47f5ae4c3510
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-03 18:47:16 +0900

    Title: 기수 정렬 구현, Time: 296ms, Memory: 78MB, Status: Passed - CodeTree

--- 240103/기수 정렬 구현/implement-radix-sort.py
+++ 240103/기수 정렬 구현/implement-radix-sort.py
@@ -2,7 +2,7 @@ n = int(input())
 nums = input().split()
 
 def radix_sort(nums):
-    for i in range(len(nums[0])):
+    for i in range(max_len - 1, -1, -1):
         temp = [[] for _ in range(10)]
 
         for num in nums:
@@ -15,6 +15,9 @@ def radix_sort(nums):
 
         nums = new_nums
     
-    return nums
+    return list(map(int, nums))
+
+max_len = len(max(nums, key=len))
+nums = ['0' * (max_len - len(num)) + num if len(num) < max_len else num for num in nums]
 
 print(*radix_sort(nums))
