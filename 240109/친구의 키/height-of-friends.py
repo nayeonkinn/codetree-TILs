bba4ae1739f664502d8bca983e1731904f3a2016
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-09 11:46:09 +0900

    Title: 친구의 키, Time: 450ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 240109/친구의 키/height-of-friends.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n, m = map(int, input().split())
+edges = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+
+for i in range(1, n + 1):
+    if not indegree[i]:
+        q.append(i)
+
+while q:
+    x = q.popleft()
+    print(x, end=' ')
+    
+    for y in edges[x]:
+        indegree[y] -= 1
+
+        if not indegree[y]:
+            q.append(y)
