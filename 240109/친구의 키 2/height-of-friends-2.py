3707d659c2b5105191f97800f3beea1cb5b1fb8f
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-09 11:54:53 +0900

    Title: 친구의 키 2, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240109/친구의 키 2/height-of-friends-2.py
+++ 240109/친구의 키 2/height-of-friends-2.py
@@ -1,12 +1,30 @@
+from collections import deque
+
 n, m = map(int, input().split())
 edges = [[] for _ in range(n + 1)]
 indegree = [0] * (n + 1)
+q = deque()
+order = []
 
 for _ in range(m):
     a, b = map(int, input().split())
     indegree[b] += 1
 
-if n == sum(indegree):
+for i in range(1, n + 1):
+    if not indegree[i]:
+        q.append(i)
+
+while q:
+    x = q.popleft()
+    order.append(x)
+
+    for y in edges[x]:
+        indegree[y] -= 1
+
+        if not edges[y]:
+            q.append(y)
+
+if n == len(order):
     print('Consistent')
 else:
     print('Inconsistent')
