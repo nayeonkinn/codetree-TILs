e626b10fb6cdb255c146d18de685baf6fca22ae2
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-10 18:07:29 +0900

    Title: 노드 맞추기, Time: 289ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240110/노드 맞추기/nodes-guessing.py
@@ -0,0 +1,36 @@
+from collections import deque 
+
+n = int(input())
+nodes = sorted(input().split())
+m = int(input())
+
+descendant = {node: [] for node in nodes}
+child = {node: [] for node in nodes}
+indegree = {node: 0 for node in nodes}
+
+q = deque()
+
+for _ in range(m):
+    x, y = input().split()
+    descendant[y].append(x)
+    indegree[x] += 1
+
+for node in nodes:
+    if not indegree[node]:
+        q.append(node)
+
+print(len(q))
+print(*q)
+
+while q:
+    x = q.popleft()
+
+    for y in descendant[x]:
+        indegree[y] -= 1
+
+        if not indegree[y]:
+            q.append(y)
+            child[x].append(y)
+
+for k, v in child.items():
+    print(k, len(v), *v)
