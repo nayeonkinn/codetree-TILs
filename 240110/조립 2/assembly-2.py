467b11626df55a89879df93e23aec62a6d356614
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-10 22:12:20 +0900

    Title: 조립 2, Time: 824ms, Memory: 47MB, Status: Passed - CodeTree

--- 240110/조립 2/assembly-2.py
+++ 240110/조립 2/assembly-2.py
@@ -14,9 +14,9 @@ input()
 piece = set(map(int, input().split()))
 
 q = deque()
-for p in piece:
-    if not indegree[p]:
-        q.append(p)
+for i in range(1, n + 1):
+    if i in piece:
+        q.append(i)
 
 while q:
     x = q.popleft()
@@ -24,7 +24,7 @@ while q:
     for y in edges[x]:
         indegree[y] -= 1
 
-        if not indegree[y]:
+        if not indegree[y] and y not in piece:
             q.append(y)
             piece.add(y)
 
