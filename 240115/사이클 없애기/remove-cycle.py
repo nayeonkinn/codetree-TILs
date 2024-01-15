4d64e3c79079f6ccec0e945c0651c8bbe44b4329
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-15 15:21:51 +0900

    Title: 사이클 없애기, Time: 521ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240115/사이클 없애기/remove-cycle.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m1, m2 = map(int, input().split())
+edges = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+q = deque()
+route = []
+
+for _ in range(m1):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    indegree[b] += 1
+
+# for _ in range(m2):
+#     a, b = map(int, input().split())
+#     edges[a].append(b)
+#     edges[b].append(a)
+#     indegree[a] += 1
+#     indegree[b] += 1
+
+for i in range(1, n + 1):
+    if not indegree[i]:
+        q.append(i)
+
+while q:
+    x = q.popleft()
+    route.append(x)
+
+    for y in edges[x]:
+        indegree[y] -= 1
+
+        if not indegree[y]:
+            q.append(y)
+
+if len(route) == n:
+    print('Yes')
+else:
+    print('No')
