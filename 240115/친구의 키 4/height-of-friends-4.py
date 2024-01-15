d1bd99f7cf1c60b2d017186bab61424f0302ede5
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-15 17:34:24 +0900

    Title: 친구의 키 4, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240115/친구의 키 4/height-of-friends-4.py
+++ 240115/친구의 키 4/height-of-friends-4.py
@@ -1,21 +1,22 @@
 from collections import deque
 
-def is_cycle(limit):
-    edges = [[] for _ in range(n + 1)]
-    indegree = [0] * (n + 1)
-    visited = [False] * (n + 1)
+def cycle(limit):
+    edges = [[] for _ in range(m + 1)]
+    indegree = [0] * (m + 1)
+    visited = [False] * (m + 1)
 
     q = deque()
-
+    
     for i in range(1, limit + 1):
         x, y = hints[i]
+
         edges[x].append(y)
         indegree[y] += 1
     
-    for i in range(1, n + 1):
-        if not indegree[i]:
-            q.append(i)
-
+    for x in range(1, n + 1):
+        if not indegree[x]:
+            q.append(x)
+        
     while q:
         x = q.popleft()
         visited[x] = True
@@ -29,10 +30,7 @@ def is_cycle(limit):
     return not all(visited[1:])
 
 n, m = map(int, input().split())
-
-hints = [()]
-for _ in range(m):
-    hints.append(tuple(map(int, input().split())))
+hints = [()] + [tuple(map(int, input().split())) for _ in range(m)]
 
 answer = 0
 lo, hi = 1, m
@@ -40,9 +38,9 @@ lo, hi = 1, m
 while lo <= hi:
     mid = (lo + hi) // 2
 
-    if is_cycle(mid):
-        answer = mid
+    if cycle(mid):
         hi = mid - 1
+        answer = mid
     else:
         lo = mid + 1
 
