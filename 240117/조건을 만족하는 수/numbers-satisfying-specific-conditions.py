1e0298f7757b702035711b973000d29dbc1fd618
Nayeon Kim <98254573+nayeonkinn@users.noreply.github.com>
2024-01-17 15:01:33 +0900

    Title: 조건을 만족하는 수, Time: 301ms, Memory: 27MB, Status: Passed - CodeTree

--- 240117/조건을 만족하는 수/numbers-satisfying-specific-conditions.py
+++ 240117/조건을 만족하는 수/numbers-satisfying-specific-conditions.py
@@ -6,57 +6,42 @@ signs = input().split()
 edges = [[] for _ in range(n + 1)]
 indegree = [0] * (n + 1)
 
-min_q, max_q = [], []
-min_answer, max_answer = [], []
-
 for i in range(n - 1):
     if signs[i] == '<':
         edges[i + 1].append(i + 2)
-        indegree[i + 2] += 1
     else:
         edges[i + 2].append(i + 1)
-        indegree[i + 1] += 1
 
-for i in range(1, n + 1):
-    if not indegree[i]:
-        heapq.heappush(min_q, i)
-        heapq.heappush(max_q, -i)
+def topological_sort(reverse):
+    pq = []
+    answer = []
 
-temp = indegree[:]
+    for i in range(1, n + 1):
+        for x in edges[i]:
+            indegree[x] += 1
 
-while min_q:
-    x = heapq.heappop(min_q)
-    min_answer.append(x)
+    for i in range(1, n + 1):
+        if not indegree[i]:
+            heapq.heappush(pq, -i if reverse else i)
+    
+    while pq:
+        x = heapq.heappop(pq) * (-1 if reverse else 1)
+        answer.append(x)
 
-    for y in edges[x]:
-        indegree[y] -= 1
+        for y in edges[x]:
+            indegree[y] -= 1
 
-        if not indegree[y]:
-            heapq.heappush(min_q, y)
+            if not indegree[y]:
+                heapq.heappush(pq, -y if reverse else y)
 
-indegree = temp
+    answer2 = [0] * n
 
-while max_q:
-    x = heapq.heappop(max_q) * -1
-    max_answer.append(x)
+    for i in range(n):
+        answer2[answer[i] - 1] = i + 1
 
-    for y in edges[x]:
-        indegree[y] -= 1
+    for num in answer2:
+        print(f'{num:03d}', end='')
+    print()
 
-        if not indegree[y]:
-            heapq.heappush(max_q, -y)
-
-min_answer2 = [0] * n
-max_answer2 = [0] * n
-
-for i in range(n):
-    min_answer2[min_answer[i] - 1] = i + 1
-    max_answer2[max_answer[i] - 1] = i + 1
-
-for num in min_answer2:
-    print(f'{num:03d}', end='')
-print()
-
-for num in max_answer2:
-    print(f'{num:03d}', end='')
-print()
+topological_sort(False)
+topological_sort(True)
